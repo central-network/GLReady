0f7e66bd936994c23c38928b2048893bf5c01926
Özgür, Dr <ozgur@phy-x.net>
2024-02-22 21:42:34 +0300

    spme

--- coffee/gl2.coffee
+++ coffee/gl2.coffee
@@ -1,24 +1,141 @@
+Object.defineProperties Math,
+    rad             : value : ( deg ) -> deg * Math.PI / 180
+    deg             : value : ( rad ) -> rad * 180 / Math.PI
+
+#? https://webgl2fundamentals.org/webgl/lessons/webgl-3d-perspective.html
+export class M4 extends Float32Array
+    
+    constructor : ( width, height, depth ) ->
+        super [ # Note: This matrix flips the Y axis so 0 is at the top.
+             2 / width,   0, 0, 0,
+             0, -2 / height, 0, 0,
+             0,  0,  2 / depth, 0,
+            -1,  1,  0,  1,
+        ]
+
+    @multiply   = ( a, b ) ->
+        a00 = a[0 * 4 + 0]; a01 = a[0 * 4 + 1]; a02 = a[0 * 4 + 2]; a03 = a[0 * 4 + 3]
+        a10 = a[1 * 4 + 0]; a11 = a[1 * 4 + 1]; a12 = a[1 * 4 + 2]; a13 = a[1 * 4 + 3]
+        a20 = a[2 * 4 + 0]; a21 = a[2 * 4 + 1]; a22 = a[2 * 4 + 2]; a23 = a[2 * 4 + 3] 
+        a30 = a[3 * 4 + 0]; a31 = a[3 * 4 + 1]; a32 = a[3 * 4 + 2]; a33 = a[3 * 4 + 3]
+
+        b00 = b[0 * 4 + 0]; b01 = b[0 * 4 + 1]; b02 = b[0 * 4 + 2]; b03 = b[0 * 4 + 3] 
+        b10 = b[1 * 4 + 0]; b11 = b[1 * 4 + 1]; b12 = b[1 * 4 + 2]; b13 = b[1 * 4 + 3] 
+        b20 = b[2 * 4 + 0]; b21 = b[2 * 4 + 1]; b22 = b[2 * 4 + 2]; b23 = b[2 * 4 + 3] 
+        b30 = b[3 * 4 + 0]; b31 = b[3 * 4 + 1]; b32 = b[3 * 4 + 2]; b33 = b[3 * 4 + 3]
+        
+        [
+            b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+            b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+            b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+            b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+            b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+            b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+            b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+            b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+            b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+            b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+            b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+            b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+            b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+            b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+            b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+            b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+        ]
+
+    translation : ( tx, ty, tz ) ->
+        [
+             1,  0,  0,  0,
+             0,  1,  0,  0,
+             0,  0,  1,  0,
+            tx, ty, tz,  1,
+        ]
+
+    xRotation   : ( angleInRadians ) ->
+        c = Math.cos angleInRadians
+        s = Math.sin angleInRadians
+
+        [
+             1,  0,  0,  0,
+             0,  c,  s,  0,
+             0, -s,  c,  0,
+             0,  0,  0,  1,
+        ]
+
+    yRotation   : ( angleInRadians ) ->
+        c = Math.cos angleInRadians
+        s = Math.sin angleInRadians
+        
+        [
+             c,  0, -s,  0,
+             0,  1,  0,  0,
+             s,  0,  c,  0,
+             0,  0,  0,  1,
+        ]
+
+    zRotation   : ( angleInRadians ) ->
+        c = Math.cos angleInRadians
+        s = Math.sin angleInRadians
+        
+        [
+             c,  s,  0,  0,
+            -s,  c,  0,  0,
+             0,  0,  1,  0,
+             0,  0,  0,  1,
+        ]
+
+    scaling     : ( sx, sy, sz ) ->
+        [
+            sx, 0,  0,  0,
+            0, sy,  0,  0,
+            0,  0, sz,  0,
+            0,  0,  0,  1,
+        ]
+
+    translate   : ( tx, ty, tz ) ->
+        @set M4.multiply this, @translation tx, ty, tz ; @
+
+    rotate      : ( rx, ry, rz ) ->
+        this
+            .xRotate rx
+            .yRotate ry
+            .zRotate rz
+    
+    scale       : ( sx, sy, sz ) ->
+        @set M4.multiply this, @scaling sx, sy, sz ; @
+        
+    xRotate     : ( angleInRadians ) ->
+        @set M4.multiply this, @xRotation angleInRadians ; @
+    
+    yRotate     : ( angleInRadians ) ->
+        @set M4.multiply this, @yRotation angleInRadians ; @
+
+    zRotate     : ( angleInRadians ) ->
+        @set M4.multiply this, @zRotation angleInRadians ; @
+    
+
 export default class GL2 extends EventTarget
 
     vertexShaderSource    : '
-    precision mediump  int;
-    precision mediump  float;
-    attribute vec3     a_Vertex;
-    attribute vec4     a_Color;
-    uniform   mat4     u_Camera;
+    attribute vec4     a_Vertex;
+    attribute vec3     a_Color;
     uniform   float    u_PointSize;
+    uniform   float    u_FudgeFactor;
+    uniform   mat4     u_Camera;
     varying   vec4     v_Color;
 
     void main() {
-        gl_Position  = u_Camera * ( vec4( a_Vertex, 1.0 ) );
-        gl_PointSize = u_PointSize;
-        v_Color      = a_Color;
+        vec4  vPos   = u_Camera * a_Vertex;
+        float zDiv   = 1.0 + vPos.z * u_FudgeFactor;
+
+        gl_Position  =  vec4( vPos.xy / zDiv, vPos.zw );
+        gl_PointSize =  u_PointSize;
+        v_Color      =  vec4( a_Color, 1.0 );
     }
     ';
 
     fragmentShaderSource  : '
-    precision mediump  int;
-    precision mediump  float;
+    precision highp    float;
     varying   vec4     v_Color;
 
     void main() {
@@ -28,6 +145,7 @@ export default class GL2 extends EventTarget
 
     stats : new Uint32Array 256
     pointCount : 0
+    rendering : yes
 
     constructor : ( canvas ) ->
 
@@ -40,10 +158,22 @@ export default class GL2 extends EventTarget
         Object.defineProperties this,
             width           : get   : -> @boundingRect.width
             height          : get   : -> @boundingRect.height
+            depth           : get   : -> @boundingRect.width / 2
             left            : get   : -> @boundingRect.x
             top             : get   : -> @boundingRect.y
-            vertical        : value : @width  / Math.PI
-            horizontal      : value : @height / Math.PI    
+
+            vFactor         : value : @width  / Math.PI
+            hFactor         : value : @height / Math.PI    
+            zFactor         : value : Math.PI / 1e2   
+
+            deltaY          : set   : ( dz ) -> @pointerZ = dz * @zFactor
+            offsetX         : set   : ( dx ) -> @pointerX = dx * @pixelRatio
+            offsetY         : set   : ( dy ) -> @pointerY = dy * @pixelRatio
+
+            translation     : value : [ 100, 100, 0 ]
+            rotation        : value : [ Math.rad(0), Math.rad(0), Math.rad(0) ]
+            scale           : value : [ 1, 1, 1 ]
+            fudgeFactor     : value : 1
 
         Object.assign @canvas,
             width           : @width  * @pixelRatio
@@ -57,14 +187,13 @@ export default class GL2 extends EventTarget
             fragmentShader  : value :   @gl.createShader( @gl.FRAGMENT_SHADER )
 
         Object.defineProperties this,
-            pointSize       : value :   new Number 20
-            points          : value :   new Number 1e6
-            vertices        : value :   new Float32Array 3 * 1e6
-            colors          : value :   new Float32Array 4 * 1e6
-            clearColor      : value :   new Float32Array [ 15/100, 17/100, 26/100, 1 ]
-            clearMask       : value :   new Number @gl.DEPTH_BUFFER_BIT | @gl.COLOR_BUFFER_BIT
-            clearDepth      : value :   new Number 1
-            camera          : value :   new Float32Array [ 1.56, 0, 0, 0, 1.73, 0, 0, 0, -1, -1, 0, 0, 4.7, 5 ]  
+            pointSize       : value :   2
+            vertices        : value :   new Float32Array 3 * 1e5
+            colors          : value :   new Float32Array 3 * 1e5
+            clearColor      : value :   new Float32Array [ 15/0xff, 17/0xff, 26/0xff, 1 ]
+            camera          : value :   new M4 @width, @height, @depth
+            clearMask       : value :   @gl.DEPTH_BUFFER_BIT | @gl.COLOR_BUFFER_BIT
+            clearDepth      : value :   1
 
         @gl.shaderSource                @vertexShader, @vertexShaderSource
         @gl.compileShader               @vertexShader
@@ -75,18 +204,21 @@ export default class GL2 extends EventTarget
         @gl.attachShader                @program, @fragmentShader
 
         @gl.enable                      @gl.DEPTH_TEST
+        @gl.enable                      @gl.CULL_FACE
         @gl.depthFunc                   @gl.LEQUAL
         @gl.clearDepth                  @clearDepth
         @gl.clearColor                  ...@clearColor
 
+        @gl.viewport                    0, 0, @canvas.width, @canvas.height
         @gl.linkProgram                 @program
         @gl.useProgram                  @program
 
-        Object.defineProperties @program,
+        Object.defineProperties this,
             a_Vertex        : value :   @gl.getAttribLocation  @program, "a_Vertex"
             a_Color         : value :   @gl.getAttribLocation  @program, "a_Color"
             u_Camera        : value :   @gl.getUniformLocation @program, "u_Camera"
             u_PointSize     : value :   @gl.getUniformLocation @program, "u_PointSize"
+            u_FudgeFactor   : value :   @gl.getUniformLocation @program, "u_FudgeFactor"
 
         @gl.bindBuffer                  @gl.ARRAY_BUFFER, @vertexBuffer
         @gl.bufferData                  @gl.ARRAY_BUFFER, @vertices, @gl.STATIC_DRAW
@@ -96,20 +228,25 @@ export default class GL2 extends EventTarget
         @gl.bindBuffer                  @gl.ARRAY_BUFFER, @colorBuffer
         @gl.bufferData                  @gl.ARRAY_BUFFER, @colors, @gl.STATIC_DRAW
         @gl.enableVertexAttribArray     @a_Color
-        @gl.vertexAttribPointer         @a_Color, 4, @gl.FLOAT, yes, 16, 0
+        @gl.vertexAttribPointer         @a_Color, 3, @gl.FLOAT, no, 12, 0
 
+        @camera.translate               ...@translation
+        @camera.rotate                  ...@rotation
+        @camera.scale                   ...@scale
+
+        @gl.uniform1f                   @u_FudgeFactor, @fudgeFactor
         @gl.uniform1f                   @u_PointSize, @pointSize
-        @gl.uniformMatrix4fv            @u_Camera, !yes, @camera
+        @gl.uniformMatrix4fv            @u_Camera, no, @camera
 
         @dump()
-        @render()
+        @bindEvents()
 
     dump        : ->
         setInterval =>
-            console.warn { @stats }
+            console.warn { @stats, this: @ }
         , 3000 ; @
    
-    reload      : ->
+    upload      : ->
         @gl.bindBuffer                  @gl.ARRAY_BUFFER, @vertexBuffer
         @gl.bufferData                  @gl.ARRAY_BUFFER, @vertices, @gl.STATIC_DRAW
         @gl.enableVertexAttribArray     @a_Vertex
@@ -118,12 +255,29 @@ export default class GL2 extends EventTarget
         @gl.bindBuffer                  @gl.ARRAY_BUFFER, @colorBuffer
         @gl.bufferData                  @gl.ARRAY_BUFFER, @colors, @gl.STATIC_DRAW
         @gl.enableVertexAttribArray     @a_Color
-        @gl.vertexAttribPointer         @a_Color, 4, @gl.FLOAT, yes, 16, 0
+        @gl.vertexAttribPointer         @a_Color, 3, @gl.FLOAT, no, 12, 0
 
     render      : =>
-        @stats[0]++
-        @gl.clear @clearMask
-        @gl.drawArrays @gl.TRIANGLES, 0, @pointCount
-        @gl.drawArrays @gl.POINTS, 0, @pointCount
+
+        if  @rendering
+            @stats[0]++
+
+            @gl.clear @clearMask
+            @gl.drawArrays @gl.TRIANGLES, 0, @pointCount
+            @gl.drawArrays @gl.POINTS, 0, @pointCount
+
         requestAnimationFrame @render
 
+
+    bindEvents  : ->
+        addEventListener "visibilitychange", =>
+            @rendering = document.visibilityState is "visible"
+    
+        addEventListener "pagehide", (e) => console.warn "onunload: quit-nonblock:", e
+        addEventListener "pageshow", (e) => e.persisted and console.warn "backtab:", e
+
+        @canvas.addEventListener "wheel", ({ @deltaY }) =>, { passive: !0 }
+        @canvas.addEventListener "pointermove", ({ @offsetX, @offsetY }) =>, { passive: !0 }
+
+
+    
--- gl2.js
+++ gl2.js
@@ -1,6 +1,122 @@
 var GL2,
   boundMethodCheck = function(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new Error('Bound instance method accessed before binding'); } };
 
+Object.defineProperties(Math, {
+  rad: {
+    value: function(deg) {
+      return deg * Math.PI / 180;
+    }
+  },
+  deg: {
+    value: function(rad) {
+      return rad * 180 / Math.PI;
+    }
+  }
+});
+
+//? https://webgl2fundamentals.org/webgl/lessons/webgl-3d-perspective.html
+export var M4 = class M4 extends Float32Array {
+  constructor(width, height, depth) {
+    super([2 / width, 0, 0, 0, 0, -2 / height, 0, 0, 0, 0, 2 / depth, 0, -1, 1, 0, 1]); // Note: This matrix flips the Y axis so 0 is at the top.
+  }
+
+  static multiply(a, b) {
+    var a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33, b00, b01, b02, b03, b10, b11, b12, b13, b20, b21, b22, b23, b30, b31, b32, b33;
+    a00 = a[0 * 4 + 0];
+    a01 = a[0 * 4 + 1];
+    a02 = a[0 * 4 + 2];
+    a03 = a[0 * 4 + 3];
+    a10 = a[1 * 4 + 0];
+    a11 = a[1 * 4 + 1];
+    a12 = a[1 * 4 + 2];
+    a13 = a[1 * 4 + 3];
+    a20 = a[2 * 4 + 0];
+    a21 = a[2 * 4 + 1];
+    a22 = a[2 * 4 + 2];
+    a23 = a[2 * 4 + 3];
+    a30 = a[3 * 4 + 0];
+    a31 = a[3 * 4 + 1];
+    a32 = a[3 * 4 + 2];
+    a33 = a[3 * 4 + 3];
+    b00 = b[0 * 4 + 0];
+    b01 = b[0 * 4 + 1];
+    b02 = b[0 * 4 + 2];
+    b03 = b[0 * 4 + 3];
+    b10 = b[1 * 4 + 0];
+    b11 = b[1 * 4 + 1];
+    b12 = b[1 * 4 + 2];
+    b13 = b[1 * 4 + 3];
+    b20 = b[2 * 4 + 0];
+    b21 = b[2 * 4 + 1];
+    b22 = b[2 * 4 + 2];
+    b23 = b[2 * 4 + 3];
+    b30 = b[3 * 4 + 0];
+    b31 = b[3 * 4 + 1];
+    b32 = b[3 * 4 + 2];
+    b33 = b[3 * 4 + 3];
+    return [b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30, b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31, b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32, b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33, b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30, b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31, b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32, b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33, b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30, b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31, b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32, b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33, b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30, b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31, b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32, b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33];
+  }
+
+  translation(tx, ty, tz) {
+    return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, tz, 1];
+  }
+
+  xRotation(angleInRadians) {
+    var c, s;
+    c = Math.cos(angleInRadians);
+    s = Math.sin(angleInRadians);
+    return [1, 0, 0, 0, 0, c, s, 0, 0, -s, c, 0, 0, 0, 0, 1];
+  }
+
+  yRotation(angleInRadians) {
+    var c, s;
+    c = Math.cos(angleInRadians);
+    s = Math.sin(angleInRadians);
+    return [c, 0, -s, 0, 0, 1, 0, 0, s, 0, c, 0, 0, 0, 0, 1];
+  }
+
+  zRotation(angleInRadians) {
+    var c, s;
+    c = Math.cos(angleInRadians);
+    s = Math.sin(angleInRadians);
+    return [c, s, 0, 0, -s, c, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+  }
+
+  scaling(sx, sy, sz) {
+    return [sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1];
+  }
+
+  translate(tx, ty, tz) {
+    this.set(M4.multiply(this, this.translation(tx, ty, tz)));
+    return this;
+  }
+
+  rotate(rx, ry, rz) {
+    return this.xRotate(rx).yRotate(ry).zRotate(rz);
+  }
+
+  scale(sx, sy, sz) {
+    this.set(M4.multiply(this, this.scaling(sx, sy, sz)));
+    return this;
+  }
+
+  xRotate(angleInRadians) {
+    this.set(M4.multiply(this, this.xRotation(angleInRadians)));
+    return this;
+  }
+
+  yRotate(angleInRadians) {
+    this.set(M4.multiply(this, this.yRotation(angleInRadians)));
+    return this;
+  }
+
+  zRotate(angleInRadians) {
+    this.set(M4.multiply(this, this.zRotation(angleInRadians)));
+    return this;
+  }
+
+};
+
 export default GL2 = (function() {
   class GL2 extends EventTarget {
     constructor(canvas) {
@@ -34,6 +150,11 @@ export default GL2 = (function() {
             return this.boundingRect.height;
           }
         },
+        depth: {
+          get: function() {
+            return this.boundingRect.width / 2;
+          }
+        },
         left: {
           get: function() {
             return this.boundingRect.x;
@@ -44,11 +165,41 @@ export default GL2 = (function() {
             return this.boundingRect.y;
           }
         },
-        vertical: {
+        vFactor: {
           value: this.width / Math.PI
         },
-        horizontal: {
+        hFactor: {
           value: this.height / Math.PI
+        },
+        zFactor: {
+          value: Math.PI / 1e2
+        },
+        deltaY: {
+          set: function(dz) {
+            return this.pointerZ = dz * this.zFactor;
+          }
+        },
+        offsetX: {
+          set: function(dx) {
+            return this.pointerX = dx * this.pixelRatio;
+          }
+        },
+        offsetY: {
+          set: function(dy) {
+            return this.pointerY = dy * this.pixelRatio;
+          }
+        },
+        translation: {
+          value: [100, 100, 0]
+        },
+        rotation: {
+          value: [Math.rad(0), Math.rad(0), Math.rad(0)]
+        },
+        scale: {
+          value: [1, 1, 1]
+        },
+        fudgeFactor: {
+          value: 1
         }
       });
       Object.assign(this.canvas, {
@@ -74,28 +225,25 @@ export default GL2 = (function() {
       });
       Object.defineProperties(this, {
         pointSize: {
-          value: new Number(20)
-        },
-        points: {
-          value: new Number(1e6)
+          value: 2
         },
         vertices: {
-          value: new Float32Array(3 * 1e6)
+          value: new Float32Array(3 * 1e5)
         },
         colors: {
-          value: new Float32Array(4 * 1e6)
+          value: new Float32Array(3 * 1e5)
         },
         clearColor: {
-          value: new Float32Array([15 / 100, 17 / 100, 26 / 100, 1])
-        },
-        clearMask: {
-          value: new Number(this.gl.DEPTH_BUFFER_BIT | this.gl.COLOR_BUFFER_BIT)
-        },
-        clearDepth: {
-          value: new Number(1)
+          value: new Float32Array([15 / 0xff, 17 / 0xff, 26 / 0xff, 1])
         },
         camera: {
-          value: new Float32Array([1.56, 0, 0, 0, 1.73, 0, 0, 0, -1, -1, 0, 0, 4.7, 5])
+          value: new M4(this.width, this.height, this.depth)
+        },
+        clearMask: {
+          value: this.gl.DEPTH_BUFFER_BIT | this.gl.COLOR_BUFFER_BIT
+        },
+        clearDepth: {
+          value: 1
         }
       });
       this.gl.shaderSource(this.vertexShader, this.vertexShaderSource);
@@ -105,12 +253,14 @@ export default GL2 = (function() {
       this.gl.compileShader(this.fragmentShader);
       this.gl.attachShader(this.program, this.fragmentShader);
       this.gl.enable(this.gl.DEPTH_TEST);
+      this.gl.enable(this.gl.CULL_FACE);
       this.gl.depthFunc(this.gl.LEQUAL);
       this.gl.clearDepth(this.clearDepth);
       this.gl.clearColor(...this.clearColor);
+      this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
       this.gl.linkProgram(this.program);
       this.gl.useProgram(this.program);
-      Object.defineProperties(this.program, {
+      Object.defineProperties(this, {
         a_Vertex: {
           value: this.gl.getAttribLocation(this.program, "a_Vertex")
         },
@@ -122,6 +272,9 @@ export default GL2 = (function() {
         },
         u_PointSize: {
           value: this.gl.getUniformLocation(this.program, "u_PointSize")
+        },
+        u_FudgeFactor: {
+          value: this.gl.getUniformLocation(this.program, "u_FudgeFactor")
         }
       });
       this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
@@ -131,21 +284,28 @@ export default GL2 = (function() {
       this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
       this.gl.bufferData(this.gl.ARRAY_BUFFER, this.colors, this.gl.STATIC_DRAW);
       this.gl.enableVertexAttribArray(this.a_Color);
-      this.gl.vertexAttribPointer(this.a_Color, 4, this.gl.FLOAT, true, 16, 0);
+      this.gl.vertexAttribPointer(this.a_Color, 3, this.gl.FLOAT, false, 12, 0);
+      this.camera.translate(...this.translation);
+      this.camera.rotate(...this.rotation);
+      this.camera.scale(...this.scale);
+      this.gl.uniform1f(this.u_FudgeFactor, this.fudgeFactor);
       this.gl.uniform1f(this.u_PointSize, this.pointSize);
-      this.gl.uniformMatrix4fv(this.u_Camera, !true, this.camera);
+      this.gl.uniformMatrix4fv(this.u_Camera, false, this.camera);
       this.dump();
-      this.render();
+      this.bindEvents();
     }
 
     dump() {
       setInterval(() => {
-        return console.warn({stats: this.stats});
+        return console.warn({
+          stats: this.stats,
+          this: this
+        });
       }, 3000);
       return this;
     }
 
-    reload() {
+    upload() {
       this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
       this.gl.bufferData(this.gl.ARRAY_BUFFER, this.vertices, this.gl.STATIC_DRAW);
       this.gl.enableVertexAttribArray(this.a_Vertex);
@@ -153,27 +313,54 @@ export default GL2 = (function() {
       this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
       this.gl.bufferData(this.gl.ARRAY_BUFFER, this.colors, this.gl.STATIC_DRAW);
       this.gl.enableVertexAttribArray(this.a_Color);
-      return this.gl.vertexAttribPointer(this.a_Color, 4, this.gl.FLOAT, true, 16, 0);
+      return this.gl.vertexAttribPointer(this.a_Color, 3, this.gl.FLOAT, false, 12, 0);
     }
 
     render() {
       boundMethodCheck(this, GL2);
-      this.stats[0]++;
-      this.gl.clear(this.clearMask);
-      this.gl.drawArrays(this.gl.TRIANGLES, 0, this.pointCount);
-      this.gl.drawArrays(this.gl.POINTS, 0, this.pointCount);
+      if (this.rendering) {
+        this.stats[0]++;
+        this.gl.clear(this.clearMask);
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, this.pointCount);
+        this.gl.drawArrays(this.gl.POINTS, 0, this.pointCount);
+      }
       return requestAnimationFrame(this.render);
+    }
+
+    bindEvents() {
+      addEventListener("visibilitychange", () => {
+        return this.rendering = document.visibilityState === "visible";
+      });
+      addEventListener("pagehide", (e) => {
+        return console.warn("onunload: quit-nonblock:", e);
+      });
+      addEventListener("pageshow", (e) => {
+        return e.persisted && console.warn("backtab:", e);
+      });
+      this.canvas.addEventListener("wheel", ({deltaY}) => {
+        this.deltaY = deltaY;
+      }, {
+        passive: !0
+      });
+      return this.canvas.addEventListener("pointermove", ({offsetX, offsetY}) => {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+      }, {
+        passive: !0
+      });
     }
 
   };
 
-  GL2.prototype.vertexShaderSource = 'precision mediump  int; precision mediump  float; attribute vec3     a_Vertex; attribute vec4     a_Color; uniform   mat4     u_Camera; uniform   float    u_PointSize; varying   vec4     v_Color; void main() { gl_Position  = u_Camera * ( vec4( a_Vertex, 1.0 ) ); gl_PointSize = u_PointSize; v_Color      = a_Color; }';
+  GL2.prototype.vertexShaderSource = 'attribute vec4     a_Vertex; attribute vec3     a_Color; uniform   float    u_PointSize; uniform   float    u_FudgeFactor; uniform   mat4     u_Camera; varying   vec4     v_Color; void main() { vec4  vPos   = u_Camera * a_Vertex; float zDiv   = 1.0 + vPos.z * u_FudgeFactor; gl_Position  =  vec4( vPos.xy / zDiv, vPos.zw ); gl_PointSize =  u_PointSize; v_Color      =  vec4( a_Color, 1.0 ); }';
 
-  GL2.prototype.fragmentShaderSource = 'precision mediump  int; precision mediump  float; varying   vec4     v_Color; void main() { gl_FragColor = v_Color; }';
+  GL2.prototype.fragmentShaderSource = 'precision highp    float; varying   vec4     v_Color; void main() { gl_FragColor = v_Color; }';
 
   GL2.prototype.stats = new Uint32Array(256);
 
   GL2.prototype.pointCount = 0;
+
+  GL2.prototype.rendering = true;
 
   return GL2;
 
